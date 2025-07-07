dc809bde7b042682e25ce4e2038bc4be7b682e56
Ahsan <rajaahs123@gmail.com>
2025-07-07 17:22:32 +0500

    Refactor App component to include counter functionality and clean up commented code

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,37 @@ import './App.css'
 import './Hero'
 import Hero from './Hero'
 
-function App() {
-  return (
-    // <> Fragment
-    // we inheriet hero code and we can also our own code using <> </> frament 
-    <>
-    <Hero/>
-    <h1>This is fragment Code </h1>
-    <p>we are learning React</p>
-    </>
-  )}
-
-
 // function App() {
-//   const [count, setCount] = useState(0)
-
-
 //   return (
+//     // <> Fragment
+//     // we inheriet hero code and we can also our own code using <> </> frament 
 //     <>
-//       <h2>The Counter App</h2>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//       <button>Add more</button>
-//       <button>Remove Button</button>
+//     <Hero/>
+//     <h1>This is fragment Code </h1>
+//     <p>we are learning React</p>
 //     </>
-//   )
-// }
+//   )}
+
+
+function App() {
+let counter =25;
+const addValue=() => {
+  console.log("value added: ",Math.random());
+  // counter = counter + 1
+}
+
+  return (
+    <>
+      <h2>The Counter App</h2>
+      <h3>The counter value is : {counter}</h3>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <button
+      onClick={addValue}>Add more</button>
+      <button>Remove Button</button>
+    </>
+  )
+}
 
 export default App
